e4fb7f13347f4623cb97a2da8033b416b2dee1c3
L1ncey <2365638692@qq.com>
2022-08-10 13:46:39 +0800

    Fheal HOTFIX

--- src/main/java/nevermind/lincey/single/Command/FoodHealCommand.java
+++ src/main/java/nevermind/lincey/single/Command/FoodHealCommand.java
@@ -21,32 +21,12 @@ public class FoodHealCommand implements CommandExecutor {
         } else {
             Player ps = (Player) sender;
             if (ps.hasPermission("single.fheal")) {
-                if (args.length == 0) {
                     ps.setFoodLevel(20);
-                    ps.sendMessage(ChatColor.AQUA + "Food-Healed " + ChatColor.YELLOW  + ps + ChatColor.AQUA  + " .");
-
-                } else if (args.length == 1) {
-                    Player pr = Bukkit.getPlayer(args[0]);
-                    boolean po = pr.isOnline();
-                    if(po){
-                        pr.setFoodLevel(20);
-                        ps.sendMessage(ChatColor.AQUA + "Food-Healed " + ChatColor.YELLOW  + pr + ChatColor.AQUA  + " .");
-
-                    } else {
-                        ps.sendMessage(ChatColor.AQUA + "Target not found.");
-
-                    }
-                } else {
-                    ps.sendMessage(ChatColor.AQUA + "Usage: /fheal [target]");
-
-                }
+                    ps.sendMessage(ChatColor.AQUA + "Food-Healed " + ChatColor.YELLOW  + ps.getName() + ChatColor.AQUA  + " .");
             } else {
                 ps.sendMessage(ChatColor.WHITE + "Unknown command.");
-
             }
-
         }
-
         return true;
     }
 }
